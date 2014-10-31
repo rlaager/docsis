73475091eb3e582ca04c22d2dfb72752ab6fb929
AdrianSimionov <Daniel.Simionov@gmail.com>
2014-10-31 10:21:52 +0100

    Small update for last index value.

--- src/docsis_symtable.h
+++ src/docsis_symtable.h
@@ -439,7 +439,7 @@ symbol_type symtable[NUM_IDENTIFIERS] =  {
 { 236,    "SNMPv3AccessViewMask",              3,      233,    (encode_hexstr),         (decode_hexstr),         2,           2             }, /* TLV 202.54.3 eRouter-I12 Annex B.4.6.3 */
 { 237,    "SNMPv3AccessViewType",              4,      233,    (encode_uchar),          (decode_uchar),          1,           2             }, /* TLV 202.54.4 eRouter-I12 Annex B.4.6.4 */
 
-/* A little more organized -> Start with 328 */
+/* A little more organized -> Start with 340 */
 
 /* Generic TLV ... we only use the limits, code and length don't matter ...*/
 { 998,    "GenericTLV",                        0, 0,           (encode_nothing),        (decode_special),        0,           0             },
