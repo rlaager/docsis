127304a82d9e44e32a4f9f5656c8e1851c459e01
AdrianSimionov <Daniel.Simionov@gmail.com>
2015-01-02 00:34:48 +0100

    Not needed anymore as it was clarified by TLV64 from PacketCable spec.

--- src/docsis_symtable.h
+++ src/docsis_symtable.h
@@ -416,8 +416,6 @@ symbol_type symtable[NUM_IDENTIFIERS] =  {
 { 173,    "SwUpgradeServer6",                  58,     0,      (encode_ip6),            (decode_ip6),            0,           0             }, /* TLV 58 MULPIv3.0-I24 Annex C.1.2.8 */
 { 174,    "SubMgmtControl6",                   63,     0,      (encode_hexstr),         (decode_hexstr),         2,           2             }, /* TLV 63 MULPIv3.0-I24 Annex C.1.1.19.5 */
 
-/*{ 156, "SnmpMibObject",                    64, 0,    (encode_nothing),     (decode_snmp_object),    1,        2048       },*/
-
 { 341,    "CMTSStaticMulticastSessionEncodings",  64,  0,      (encode_nothing),        (decode_aggregate),      0,           0             }, /* TLV 64 MULPIv3.0-I24 Annex C.1.1.27 */
 /* TODO For TLV 64.1 and 64.2 encoding as IPv4 is a temporary solution, another encoder needs to be created to support both IPv4 and IPv6 */
 { 342,    "CMTSStaticMulticastSessionGroup",   1,      341,    (encode_ip),             (decode_ip_ip6),         0,           0             }, /* TLV 64.1 MULPIv3.0-I24 Annex C.1.1.27.1 */
