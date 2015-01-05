775fe443d3cb717bb8b5f49b2c2c13e33dce8150
AdrianSimionov <Daniel.Simionov@gmail.com>
2015-01-05 23:45:41 +0100

    Testing why Travis fails remote.

--- tests/RunTests.sh
+++ tests/RunTests.sh
@@ -10,7 +10,7 @@ do
   $DOCSIS -e $TEST.txt $KEYFILE $TEST.cm.new
   if [ ! -f $TEST.cm.new ]; then
     echo "Test $TEST failed to create CM file on first pass.";
-    exit 1;
+    exit -1;
   fi
   MD5SUM_1=$(md5sum $TEST.cm)
   MD5SUM_1_array=($MD5SUM_1)
@@ -18,12 +18,12 @@ do
   MD5SUM_2_array=($MD5SUM_2)
   if [ "${MD5SUM_1_array[0]}" != "${MD5SUM_2_array[0]}" ]; then
     echo "Test $TEST created a wrong CM file on first pass.";
-    exit 1;
+    exit -1;
   fi
   $DOCSIS -d $TEST.cm.new > $TEST.conf.new
   if [ ! -f $TEST.conf.new ]; then
     echo "Test $TEST failed to create a conf file on second pass.";
-    exit 1;
+    exit -1;
   fi
   MD5SUM_1=$(md5sum $TEST.conf)
   MD5SUM_1_array=($MD5SUM_1)
@@ -31,12 +31,12 @@ do
   MD5SUM_2_array=($MD5SUM_2)
   if [ "${MD5SUM_1_array[0]}" != "${MD5SUM_2_array[0]}" ]; then
     echo "Test $TEST created a wrong CONF file on second pass.";
-    exit 1;
+    exit -1;
   fi
   $DOCSIS -e $TEST.conf.new $KEYFILE $TEST.cm.new
   if [ ! -f $TEST.cm.new ]; then
     echo "Test $TEST failed to create CM file on third pass.";
-    exit 1;
+    exit -1;
   fi
   MD5SUM_1=$(md5sum $TEST.cm)
   MD5SUM_1_array=($MD5SUM_1)
@@ -44,7 +44,7 @@ do
   MD5SUM_2_array=($MD5SUM_2)
   if [ "${MD5SUM_1_array[0]}" != "${MD5SUM_2_array[0]}" ]; then
     echo "Test $TEST created a wrong CM file on third pass.";
-    exit 1;
+    exit -1;
   fi
   rm $TEST*.new
 done
